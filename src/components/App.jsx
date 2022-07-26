73fb15ae43a0cee794cfedeed80b145605cc0e73
RalphHenryDominisac <rhldominisac@addu.edu.ph>
2022-07-26 19:42:02 +0800

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function App(){
+
+    return(
+        <div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+            <p style={{textAlign: 'center'}}>LOGO MARSEEYS IMAGE HERE</p>
+
+            <p class="subsystemTitle"> Sales Ordering System (v.1.0) </p>
+
+        </div>
+
+        
+
+    );
+
+}
+
+export default App;
