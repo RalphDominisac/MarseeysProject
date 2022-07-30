97e6bf3bbcc866a62c7cc50cd66759cd56cb41b3
RalphHenryDominisac <rhldominisac@addu.edu.ph>
2022-07-30 23:20:02 +0800

    July 30 push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import SignInPOS from './SignInPOS';
+
+
 
 function App(){
 
@@ -11,9 +14,20 @@ function App(){
         <br></br>
         <br></br>
         
-            <p style={{textAlign: 'center'}}>LOGO MARSEEYS IMAGE HERE</p>
+            {/* <p style={{textAlign: 'center'}}>LOGO MARSEEYS IMAGE HERE</p> */}
+            <img id="marseeyslogo"src="images/Asset 11.png" alt="Marseeys logo" class="center" />
 
             <p class="subsystemTitle"> Sales Ordering System (v.1.0) </p>
+
+            <SignInPOS />
+          
+           
+
+       
+
+            
+
+          
 
         </div>
 
