24590b8a3ce5fdf34253e04c056a254418a83b74
Shaun Leonar <sbcleonar@addu.edu.ph>
2022-09-12 12:04:08 +0800

    - Optimized flow of information for ordering items. Ordering now uses the IDs of the menu items.

---
+++ Rest API/Point of Sale/marpos/src/main/java/com/example/marpos/dto/order/DeliveryRequest.java
@@ -0,0 +1,30 @@
+package com.example.marpos.dto.order;
+
+import com.example.marpos.enumeration.DeliveryMethod;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryRequest {
+    @NotNull(message = "Customer name cannot be null")
+    @Size(min = 2, message = "Please input a customer name")
+    private String customer;
+
+    @NotEmpty(message = "Please provide a list of items")
+    private List<Integer> contents;
+
+    @NotNull(message = "Address cannot be null")
+    @Size(min = 1, message = "Please input an address")
+    private String address;
+
+    @NotNull(message = "Please provide a delivery method")
+    private DeliveryMethod method;
+}
---
+++ Rest API/Point of Sale/marpos/src/main/java/com/example/marpos/dto/order/DineInRequest.java
@@ -0,0 +1,25 @@
+package com.example.marpos.dto.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DineInRequest {
+    @NotNull(message = "Please input a customer name")
+    @Size(min = 2, message = "Please input a customer name")
+    private String customer;
+
+    @NotEmpty(message = "Please provide a list of items")
+    private List<Integer> contents;
+
+    @NotNull(message = "Please provide a table number")
+    private int tableNo;
+}
