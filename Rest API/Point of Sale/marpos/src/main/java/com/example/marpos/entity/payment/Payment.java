e350c729f1a4c566394348b16640a3c9405cf7bd
Shaun Leonar <sbcleonar@addu.edu.ph>
2022-09-14 12:15:23 +0800

    - Added created date to payment to enable cross-referencing

--- Rest API/Point of Sale/marpos/src/main/java/com/example/marpos/entity/payment/Payment.java
+++ Rest API/Point of Sale/marpos/src/main/java/com/example/marpos/entity/payment/Payment.java
@@ -3,9 +3,12 @@ package com.example.marpos.entity.payment;
 import com.example.marpos.entity.order.Order;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.time.LocalDateTime;
 
 @Data
 @Document(collection = "Payments")
@@ -17,11 +20,14 @@ public class Payment {
     private Order order;
     private double amount;
     private double change;
+    @CreatedDate
+    private LocalDateTime time;
 
     public Payment(int id, Order order, double amount, double change) {
         this.id = id;
         this.order = order;
         this.amount = amount;
         this.change = change;
+        this.time = LocalDateTime.now();
     }
 }
