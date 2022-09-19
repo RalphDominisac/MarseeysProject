2a3d31291ca680a710d112cbf78d2f35ce55972b
RalphHenryDominisac <rhldominisac@addu.edu.ph>
2022-09-20 03:08:45 +0800

    small fixes and trial and error of add and delete menu item to cart

--- src/components/HomeOrderPage.jsx
+++ src/components/HomeOrderPage.jsx
@@ -196,6 +196,41 @@ const mdTheme = createTheme({
 
   export default function HomeOrderPage() {
 
+
+    // ADD AND DELETE TRIAL AND ERROR
+    // const { products } = data;
+    // const [cartItems, setCartItems] = useState([]);
+
+    // const onAdd = (product) => {
+    //   const exist = cartItems.find((x) => x.id === product.id);
+    //   if (exist) {
+    //     setCartItems(
+    //       cartItems.map((x) =>
+    //         x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+    //       )
+    //     );
+    //   } else {
+    //     setCartItems([...cartItems, { ...product, qty: 1 }]);
+    //   }
+    // };
+
+    // const onRemove = (product) => {
+    //   const exist = cartItems.find((x) => x.id === product.id);
+    //   if (exist.qty === 1) {
+    //     setCartItems(cartItems.filter((x) => x.id !== product.id));
+    //   } else {
+    //     setCartItems(
+    //       cartItems.map((x) =>
+    //         x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+    //       )
+    //     );
+    //   }
+    // };
+
+
+
+
+
     const [chickenFood, setChickenFood] = useState(null);
 
     // Update the prices for each menu items (Sept 7 2022 received new menu pics)
