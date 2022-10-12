de13da83ca2d981108e7afd4cf5051e04dbe3a75
alexandra <97576137+alexandralanorias@users.noreply.github.com>
2022-10-12 04:02:54 -0400

    Axios overhauling

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MarseeysProject: `pos-axios` branch
 
 reactjs directory: `src\main\resources\webapp\reactjs`
+useful resource for api requests by shaun: `https://warm-shore-95735.herokuapp.com/swagger-ui.html`
--- src/main/resources/webapp/reactjs/src/components/categoryButtons/BBQButtons.jsx
+++ src/main/resources/webapp/reactjs/src/components/categoryButtons/BBQButtons.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {useEffect} from 'react';
 import Axios from "../../axios/Axios.tsx";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
@@ -20,6 +21,9 @@ function preventDefault(event) {
   event.preventDefault();
 }
 
+// url for axios methods
+const INITIAL_URL = process.env.NEXT_PUBLIC_INITIAL_URL;
+
 export default function BBQButtons(props) {
   const axios = new Axios();
   
@@ -32,6 +36,32 @@ export default function BBQButtons(props) {
    };
 
  const [modalOpenAddNewItemBBQ, setModalOpenAddNewItemBBQ] = useState(false);
+
+ // axios modules
+
+ // NOT YET FINAL!!!
+ {/*const getVerticalBarGraphData = () => {
+  axios.getPost(
+    `${INITIAL_URL}/expense/vertical-bar-graph`,
+    {
+      toDate: toDate.add(1, "month").date(0),
+      fromDate: fromDate,
+    },
+    getVerticalBarGraphDataOnSuccess
+  );
+};
+
+// NOT YET FINAL!!!
+ useEffect(() => {
+  getVerticalBarGraphData();
+  getDonutGraphData();
+  getLineGraphData();
+  getTablesBarGraphData();
+  getOrdersServed();
+  getServingTypeGraphData();
+}, []);*/}
+
+// end of axios modules
 
 
   return (
